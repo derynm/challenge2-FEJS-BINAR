6e8f8713b05fd9daf7dfff01a20b37f9342d1a14
Dery <69612416+derynm@users.noreply.github.com>
2022-03-07 23:13:04 +0700

    soal 1

---
+++ soal1.js
@@ -0,0 +1,12 @@
+function changeWord(selectedText,changedText,text) {
+    let hasil =  text.replace(selectedText,changedText)
+
+    console.log(hasil)
+}
+
+const kalimat1 = "Andini sangat mencintai kamu selamanya"
+const kalimat2 = "Gunung bromo tak akan mampu menggambarkan besarnya cintaku padamu"
+
+console.log(changeWord("mencintai","membenci",kalimat1))
+console.log(changeWord("bromo","semeru",kalimat2))
+
