149765a20bcbf5d68025e043d328a3aa9f1a3946
Dery <69612416+derynm@users.noreply.github.com>
2022-03-07 23:47:02 +0700

    soal 2

---
+++ soal2.js
@@ -0,0 +1,26 @@
+let checkTypeNumber = (givenNumber) => {
+    if ((typeof givenNumber==="number")) {
+        if (givenNumber%2===0) {
+            return "GENAP"
+        }
+        else if (givenNumber%2===1) {
+            return "GANJIL"
+        }
+    }
+
+    else if ((typeof givenNumber === "undefined")) {
+        return "Bro where is the paramater?"
+    }
+
+    else {
+        return "invalid data type"
+    }
+
+}
+
+console.log(checkTypeNumber(10))
+console.log(checkTypeNumber(3))
+console.log(checkTypeNumber("3"))
+console.log(checkTypeNumber({}))
+console.log(checkTypeNumber([]))
+console.log(checkTypeNumber())
